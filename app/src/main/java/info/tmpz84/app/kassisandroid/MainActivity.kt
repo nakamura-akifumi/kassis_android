29ba4ac4fe0614bba79839d989ea23def0e6777f
Akifumi NAKAMURA <tmpz84@gmail.com>
2018-09-05 23:24:27 +0900

    androidからrtm apiを使ってslackに接続する実装。

--- app/src/main/java/info/tmpz84/app/kassisandroid/MainActivity.kt
+++ app/src/main/java/info/tmpz84/app/kassisandroid/MainActivity.kt
@@ -1,9 +1,10 @@
 package info.tmpz84.app.kassisandroid
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+
 
 const val MY_REQUEST_CODE = 0
 
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val workButton: Button = findViewById(R.id.workButton)
+        val workButton: Button = findViewById(R.id.workButton) as Button
         workButton.setOnClickListener {
 
             val intent: Intent = Intent(this, WorkActivity::class.java)
@@ -22,6 +23,11 @@ class MainActivity : AppCompatActivity() {
 
             startActivityForResult(intent, MY_REQUEST_CODE)
         }
+
+        val selectButton: Button = findViewById(R.id.selectButton) as Button
+        selectButton.setOnClickListener {
+        }
+
 
     }
 }
